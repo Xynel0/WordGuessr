35cfe07b02bcdc04d83dab337ee7f223c68976de
Xinelo Kenner dy Olympikus <261785386+Xynel0@users.noreply.github.com>
2026-07-16 00:06:45 -0300

    Update wordguessr.py

--- wordguessr.py
+++ wordguessr.py
@@ -26,6 +26,12 @@ while True:
                         print_chances = 'infinitas'
                     else:
                         print_chances = chances
+                    if len(chutes_errados) >= 1:
+                        print()
+                        print('Chutes errados:')
+                        for chute in chutes_errados:
+                            print(chute, end = ' ')
+                        print()
                     for i in palavra:
                         if i in chutes_certos:
                             print(i, end=' ')
@@ -34,17 +40,23 @@ while True:
                     print('')
                     b = True
                     for i in palavra:
-                        if i not in chutes_certos:
+                        if i not in chutes_certos and palavra not in chutes_certos:
                             b = False
                     if b == True:
-                        print(f"a palavra era {palavra}! você ganhou!\nsó teve que tentar {tentativas} vezes! parabens!!")
+                        if tentativas > 1:
+                            print(f"A palavra era {palavra}! Você ganhou!\nSó teve que tentar {tentativas} vezes! Parabéns!!")
+                        elif tentativas == 1:
+                            print(f"A palavra era {palavra}! Você ganhou!\nSó teve que tentar uma vez! Parabéns!!")
                         break
-                    letra = func.v_input(f"insira uma letra(você tem {print_chances} chances):\n", chutes_certos, chutes_errados)
-                    if letra in palavra:
+                    letra = func.v_input(f"Insira uma letra(você tem {print_chances} chances):\n", chutes_certos, chutes_errados, len(palavra))
+                    if letra in palavra or letra == palavra:
                         chutes_certos.append(letra)
                     else:
-                        chutes_errados.append(letra)
-                        chances -= 1
+                        if letra == palavra:
+                            b = True
+                        else:
+                            chutes_errados.append(letra)
+                            chances -= 1
                     tentativas = len(chutes_certos) + len(chutes_errados)
                 if b == False:
                     print(f"você perdeu!\na palavra era {palavra}.")
@@ -70,5 +82,6 @@ while True:
                 for i in ['abacate', 'abacaxi', 'abelha', 'acerola', 'agulha', 'alface', 'alho', 'amizade', 'ancora', 'anel', 'aranha', 'baleia', 'balao', 'banco', 'barco', 'barraca', 'batata', 'beijo', 'berco', 'biscoito', 'boneca', 'botao', 'brinco', 'bruxa', 'cabana', 'cabide', 'caixote', 'calca', 'caneca', 'canoa', 'canudo', 'capivara', 'castelo', 'cebola', 'cenoura', 'cereja', 'chafariz', 'chaveiro', 'chinelo', 'chocalho', 'chuchu', 'cegonha', 'coelho', 'cogumelo', 'colcha', 'colmeia', 'coqueiro', 'coruja', 'cortina', 'gaveta', 'girafa', 'girassol', 'goiaba', 'golfinho', 'gorila', 'grade', 'gravata', 'grelha', 'grilo', 'gruta', 'guaxinim', 'harpa', 'hiena', 'hipopotamo', 'horta', 'helice', 'igreja', 'iogurte', 'jacare', 'jali', 'jarra', 'jasmim', 'javali', 'joaninha', 'jornal', 'judo', 'jumento', 'juriti', 'lagarta', 'lagosta', 'lanche', 'lanterna', 'leao', 'leopardo', 'lesma', 'lagartixa', 'limao', 'lince', 'lixeira', 'lousa', 'lobo', 'lontra', 'lula', 'macaco', 'madeira', 'mastro', 'medalha', 'mochila', 'moinho', 'morcego']:
                     arq.write(i+'\n')
             current_path = default_path
+            print("sucesso!")
     else:
         print("opção inválida. insira um dos números abaixo.")
